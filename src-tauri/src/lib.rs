64dfac8d1a8a65bb16eefea5af3825f4dd259b4a
Keigo Nakatani <k5n@users.noreply.github.com>
2025-07-04 21:58:19 +0900

    feat: add stronghold plugin and HWID generation
    - Updated Cargo.toml to include `tauri-plugin-stronghold`, `rust-argon2`, and `machineid-rs` dependencies.
    - Modified default.json capabilities to include `stronghold:default` permission.
    - Implemented `get_hwid` function to generate a hardware ID using machineid-rs.
    - Integrated stronghold plugin in the Tauri application to hash the generated HWID using Argon2 for secure key generation.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,9 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+// cSpell:words HWID
 mod migrations;
 
 use dotenvy::from_filename;
+use machineid_rs::{Encryption, HWIDComponent, IdBuilder};
 use migrations::get_migrations;
 use std::env;
 
@@ -20,6 +22,18 @@ fn get_db_name() -> String {
     env::var("PUBLIC_APP_DB_NAME").unwrap_or_else(|_| "app.db".to_string())
 }
 
+/// Generates a hardware ID (HWID) for the system.
+fn get_hwid() -> String {
+    const HWID_KEY: &str = "3f8b0c3b519340974b0fa3aad09939402d969f52f1d84a4abac56f17e32623ba";
+    IdBuilder::new(Encryption::SHA256)
+        .add_component(HWIDComponent::SystemID)
+        .add_component(HWIDComponent::OSName)
+        .add_component(HWIDComponent::MachineName)
+        .add_component(HWIDComponent::Username)
+        .build(HWID_KEY)
+        .unwrap_or_else(|_| HWID_KEY.to_string())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let db_name = get_db_name();
@@ -27,6 +41,29 @@ pub fn run() {
     println!("Using database URL: {}", db_url);
 
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_stronghold::Builder::new(|_| {
+                // HWID is also hash, but it is only used to generate a unique identifier for the machine.
+                // Argon2 is a memory-hard function that is designed to be slow and resistant to brute-force attacks.
+                use argon2::{hash_raw, Config, Variant, Version};
+
+                let config = Config {
+                    lanes: 4,
+                    mem_cost: 10_000,
+                    time_cost: 10,
+                    variant: Variant::Argon2id,
+                    version: Version::Version13,
+                    ..Default::default()
+                };
+                let salt = "kotonoha".as_bytes();
+                let password = get_hwid();
+                let key =
+                    hash_raw(password.as_ref(), salt, &config).expect("failed to hash password");
+
+                key.to_vec()
+            })
+            .build(),
+        )
         .plugin(tauri_plugin_fs::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
