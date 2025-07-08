710e66eb94a38b71f0c5df29045068b389dce7d6
Keigo Nakatani <k5n@users.noreply.github.com>
2025-07-08 09:08:06 +0900

    Move password_hash_function and get_hwid to stronghold.rs for better separation of concerns

---
+++ src-tauri/src/stronghold.rs
@@ -0,0 +1,32 @@
+// cSpell:words HWID
+use argon2::{hash_raw, Config, Variant, Version};
+use machineid_rs::{Encryption, HWIDComponent, IdBuilder};
+
+/// Generates a hardware ID (HWID) for the system.
+pub fn get_hwid() -> String {
+    const HWID_KEY: &str = "3f8b0c3b519340974b0fa3aad09939402d969f52f1d84a4abac56f17e32623ba";
+    IdBuilder::new(Encryption::SHA256)
+        .add_component(HWIDComponent::SystemID)
+        .add_component(HWIDComponent::OSName)
+        .add_component(HWIDComponent::MachineName)
+        .add_component(HWIDComponent::Username)
+        .build(HWID_KEY)
+        .unwrap_or_else(|_| HWID_KEY.to_string())
+}
+
+/// password hashing function for stronghold plugin
+pub fn password_hash_function(_password: &str) -> Vec<u8> {
+    let config = Config {
+        lanes: 4,
+        mem_cost: 10_000,
+        time_cost: 10,
+        variant: Variant::Argon2id,
+        version: Version::Version13,
+        ..Default::default()
+    };
+    let salt = "kotonoha".as_bytes();
+    let password = get_hwid();
+    hash_raw(password.as_ref(), salt, &config)
+        .expect("failed to hash password")
+        .to_vec()
+}
