afeffa2e7fab453dc0500866088c9fc454cc1b00
Keigo Nakatani <k5n@users.noreply.github.com>
2025-07-29 09:45:08 +0900

    fix: refactor logging plugin configuration to conditionally set targets and levels based on debug assertions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -44,12 +44,21 @@ pub fn run() {
                 .add_migrations(&db_url, get_migrations())
                 .build(),
         )
-        .plugin(
+        .plugin(if cfg!(debug_assertions) {
             tauri_plugin_log::Builder::new()
+                .clear_targets()
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::Stdout,
+                ))
+                .level(log::LevelFilter::Debug)
+                .build()
+        } else {
+            tauri_plugin_log::Builder::new()
+                .level(log::LevelFilter::Info)
                 .max_file_size(1 * 1024 * 1024)
                 .rotation_strategy(tauri_plugin_log::RotationStrategy::KeepSome(5))
-                .build(),
-        )
+                .build()
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             analyze_sentence_with_llm,
